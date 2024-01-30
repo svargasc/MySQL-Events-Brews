ec02864e5a0f8cd2538cec3abcc4cb36e7b64fcc
svargasc <117184778+svargasc@users.noreply.github.com>
2024-01-30 08:50:33 -0500

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,11 +3,15 @@ import { createPool } from "mysql2/promise";
 
 export const pool = createPool(
   {
-    host: "localhost",
-    user: "root",
-    password: "Apto1404",
-    database: "eventsBrews",
+    host: 'bu7evesmyo0gtb3n64u8-mysql.services.clever-cloud.com',
+    database: 'bu7evesmyo0gtb3n64u8',
+    user: 'uohrfzc8hkpyk0qm',
+    password: '7tU6dFyy4nba0nf8PgBz',
+    port: 3306,
     multipleStatements: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
   console.log("Database is connected")
 );
